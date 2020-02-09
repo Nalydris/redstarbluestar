1f57f03da13f72d50c4daadffe00f38db617aa93
Nalydris <47084326+Nalydris@users.noreply.github.com>
2020-02-09 14:08:00 -0600

    Viridian blackboard Burn info corrected

--- text.asm
+++ text.asm
@@ -918,7 +918,7 @@ _ViridianBlackboardPrlzText::
 
 _ViridianBlackboardBurnText::
 	text "A burn reduces"
-	line "power and speed."
+	line "ATTACK by half."
 	cont "It also causes"
 	cont "ongoing damage."
 
